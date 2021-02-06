fcd2ab2cc285356e15249fc8ed0ca74b438ecbca
serdar tekin <kara_bulu@hotmail.com>
2021-02-06 19:37:38 +0300

    Revert "Revert "Proje dosyası ekle.""
    This reverts commit 058990eb04cc1a2627f0c0f9275b0cf63b392521.

---
+++ ICheckRealGamer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OyunSatis
+{
+    interface ICheckRealGamer
+    {
+        bool CheckGamerInfo(Gamer gamer);
+    }
+}
---
+++ IGameService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OyunSatis
+{
+    interface IGameService
+    {
+        void Sales(Gamer gamer, Game game);
+    }
+}
