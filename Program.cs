97fdc8f0316f6108bcb36e02d2580755e7eb6cf4
serdar tekin <kara_bulu@hotmail.com>
2021-02-06 19:37:24 +0300

    Revert "Revert "Proje dosyası ekle.""
    This reverts commit 058990eb04cc1a2627f0c0f9275b0cf63b392521.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OyunSatis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gamer serdar = new Gamer();
+            serdar.FirstName = "Serdar";
+            serdar.LastName = "Tekin";
+            serdar.DateOfBirth = new DateTime(1996,9,13);
+            serdar.IdNo = "01011010110";
+
+            Game gtav = new Game();
+            gtav.GameName = "GTA";
+            gtav.GamePrice = 385;
+
+            //GamerManager gamerManager = new GamerManager(new CheckRealGamer());
+            //gamerManager.Add(serdar);
+
+            GameManager gameManager = new GameManager();
+            gameManager.Sales(serdar,gtav);
+        }
+    }
+}
---
+++ TestCheckGamer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OyunSatis
+{
+    class TestCheckGamer : ICheckRealGamer
+    {
+        public bool CheckGamerInfo(Gamer gamer)
+        {
+            return true;
+        }
+    }
+}
