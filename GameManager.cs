57978a926d254aa02fdc7abb020b6704e54e6263
serdar tekin <kara_bulu@hotmail.com>
2021-02-06 19:24:40 +0300

    Proje dosyası ekle.

---
+++ GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OyunSatis
+{
+    class GameManager : IGameService
+    {
+        public void Sales(Gamer gamer, Game game)
+        {
+            Console.WriteLine("Sayın " + gamer.FirstName + " " + gamer.LastName + " " + game.GameName + " " + "Adlı Oyunu" + " " + game.GamePrice +"TL " + "Karşılığında Satın Aldınız.");
+        }
+    }
+}
